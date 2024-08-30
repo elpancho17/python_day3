19e0895ff2899d244ed86d51fb5f9bdf55901074
Lara <alara50@cps.k12.il.us>
2024-08-30 09:23:25 -0500

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,42 @@
+# comments are used to explain the code
+# this is a comment it will not be executed
+# comments are notes to the programmer
+# print function allows you to print a message to the console
+print("day 3 paython programming")
+# print function allows us to make an output to the console 
+# imput function allows you to get input from the user
+# input will return a string
+# hi= input("what is your name?")
+# print("hello " + hi )
+#name is the variable that will store the input from the user
+#a variable is a container that stores data
+
+hi= input("what is your age?")
+# ask the user for their age
+print("so your are " + hi + " years old")
+# print out the age the user with their name
+
+#concatenation is process of joining strings together
+
+hello= input ("where do you live")
+#ask for their home address
+print("so you live at " + hello )
+#print out the home address of the user
+
+phone= input ("what is your phone number")
+#ask of their phone number
+print ("so your number is " + phone)
+#print out the phone number of the user
+
+
+
+#data types variables can store different types of data
+#strings are text data -- string data type
+name = "pancho" #whenever you see text in quotes it is a string
+#intergers are whole numbers
+age= 25 #no quotes around the number
+#floats are decimal numbers
+height= 5.7 #a number with a decimal point
+#boolean are true or false values
+is_student = True #true or false
+#lists are a collection of data
